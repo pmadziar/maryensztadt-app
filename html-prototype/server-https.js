c68f98413e469d47e03ae6d4d3a3f5b1b1d8bee0
Pawel Madziar <pmadziar@madziar.com>
2016-11-15 12:06:45 +0000

    on aws

--- html-prototype/server-https.js
+++ html-prototype/server-https.js
@@ -1,15 +1,19 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
 var fs = require('fs');
 var https = require('https');
-var app = require('express')();
+
 var options = {
    key  : fs.readFileSync('/etc/letsencrypt/archive/app.madziar.com/privkey1.pem'),
-   cert : fs.readFileSync('/etc/letsencrypt/archive/app.madziar.com/cert1.pem')
+   cert : fs.readFileSync('/etc/letsencrypt/archive/app.madziar.com/fullchain1.pem')
+
 };
 
-app.get('/', function (req, res) {
-   res.send('Hello World!');
-});
+app.use(express.static(path.join(__dirname, 'dist')));
 
 https.createServer(options, app).listen(443, function () {
    console.log('Started!');
 });
+
